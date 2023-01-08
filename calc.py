d5c741a364c24c235ba88649d54a6cc693b9d9a0
facior <lukasz.kubieniec00@gmail.com>
2023-01-08 21:04:05 +0100

    calc

---
+++ calc.py
@@ -0,0 +1,17 @@
+first_number1 = int(input("Write first number "))
+second_number1 = int(input("Write second number "))
+action1 = input("Choose action (*,/,+,-)")
+
+
+def select_action(first_number1, second_number1, action1):
+    if action1 == '*':
+        print('Your score is ' + str(first_number1 * second_number1))
+    elif action1 == '/':
+        print('Your score is ' + str(first_number1 / second_number1))
+    elif action1 == '+':
+        print('Your score is ' + str(first_number1 + second_number1))
+    elif action1 == '-':
+        print('Your score is ' + str(first_number1 - second_number1))
+
+
+select_action(first_number1, second_number1, action1)
