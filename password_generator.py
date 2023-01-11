97b4eafb80927d0110ba72a6f8b63ff78857b0a8
facior <lukasz.kubieniec00@gmail.com>
2023-01-11 01:55:50 +0100

    Password Generator

--- password_generator.py
+++ password_generator.py
@@ -1,13 +1,21 @@
 import random
 import string
 
-password_lenght = int(input("How long you want your password to be ? "))
-many_password = int(input("How many password you need ? "))
-def password_generator(password_lenght):
-    return ''.join(
-        random.choices(
-            string.ascii_letters + string.ascii_uppercase + string.digits + string.punctuation, k= password_lenght
+password_lenght = int(input("How many characters do you want in your password? "))
+number_of_passwords = int(input("How many password you need ? "))
+def password_generator(password_lenght, number_of_passwords):
+    passwords = []
+    for i in range(number_of_passwords):
+        password = ''.join(
+            random.choices(string.ascii_letters + string.ascii_uppercase + string.digits + string.punctuation, k = password_lenght
+            )
         )
-    )
+        passwords.append(password)
+    return passwords
 
-print(password_generator(password_lenght))
+passwords = password_generator(password_lenght, number_of_passwords)
+
+with open("passwords.txt", "w") as file:
+    for password in passwords:
+        file.write(password + "\n")
+print("Passwords have been saved to 'passwords.txt'")
